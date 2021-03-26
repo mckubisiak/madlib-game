bad9ec8116ca0007b1b4a2506dd1303cb17c0da2
Kubi Kubisiak <mckubisiak@gmail.com>
2021-03-26 15:15:35 -0700

    added intial java

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 // import functions and grab DOM elements
 
+const input1 = document.getElementById('input1');
+const input2 = document.getElementById('input2');
+const button = document.getElementById('button');
+const output1 = document.getElementById('noun1');
+const output2 = document.getElementById('verb1');
+const output3 = document.getElementById('noun2');
+
+ 
+button.addEventListener('click', () => {
+    output1.textContent = input1.value;
+    output3.textContent = input1.value;
+    output2.textContent = input2.value;
+
+});
+
+
+
 // initialize state
 
 // set event listeners to update state and DOM
