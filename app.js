d66c1da228424a671640d645acc2b8dd57248b6c
Rue Williams <19MEDIAFOOD81@GMAIL.COM>
2021-03-27 14:20:20 -0700

    CSS update, reset button, hide/show fixed

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-// import functions and grab DOM elements
-
+//DOM Element input variables
 const input1 = document.getElementById('input1');
 const input2 = document.getElementById('input2');
 const input3 = document.getElementById('input3');
@@ -7,7 +6,9 @@ const input4 = document.getElementById('input4');
 const input5 = document.getElementById('input5');
 const input6 = document.getElementById('input6');
 const input7 = document.getElementById('input7');
-const button = document.getElementById('button');
+const buttonInput = document.getElementById('button-input');
+
+//DOM Element story variables
 const output1 = document.getElementById('adj1');
 const output2 = document.getElementById('noun2');
 const output3 = document.getElementById('location3');
@@ -15,9 +16,14 @@ const output4 = document.getElementById('noun4');
 const output5 = document.getElementById('verb5');
 const output6 = document.getElementById('verb6');
 const output7 = document.getElementById('adj7');
+const buttonReset = document.getElementById('button-reset');
 
- 
-button.addEventListener('click', () => {
+//div elements to hide or unhide
+const divStory = document.getElementById('story');
+const divInput = document.getElementById('inputs');
+
+// set event listeners to update state and DOM
+buttonInput.addEventListener('click', () => {
     output1.textContent = input1.value;
     output2.textContent = input2.value;
     output3.textContent = input3.value;
@@ -25,13 +31,11 @@ button.addEventListener('click', () => {
     output5.textContent = input5.value;
     output6.textContent = input6.value;
     output7.textContent = input7.value;
-    const div = document.getElementById('story');
-    div.classList.remove('hidden');
-
+    divStory.classList.remove('hidden');
+    divInput.classList.add('hidden');
 });
 
-
-
-// initialize state
-
-// set event listeners to update state and DOM
+buttonReset.addEventListener('click', () => {
+    divInput.classList.remove('hidden');
+    divStory.classList.add('hidden');
+});
