2cc13a5b6f1bec41f5abf95f334ed91bfa321808
Rue Williams <19MEDIAFOOD81@GMAIL.COM>
2021-03-27 13:32:46 -0700

    fixed input/output issue and eslint errors

--- app.js
+++ app.js
@@ -8,17 +8,23 @@ const input5 = document.getElementById('input5');
 const input6 = document.getElementById('input6');
 const input7 = document.getElementById('input7');
 const button = document.getElementById('button');
-const output1 = document.getElementById('noun1');
-const output4 = document.getElementById('noun1');
-const output5 = document.getElementById('noun1');
-const output2 = document.getElementById('verb1');
-const output3 = document.getElementById('noun2');
+const output1 = document.getElementById('adj1');
+const output2 = document.getElementById('noun2');
+const output3 = document.getElementById('location3');
+const output4 = document.getElementById('noun4');
+const output5 = document.getElementById('verb5');
+const output6 = document.getElementById('verb6');
+const output7 = document.getElementById('adj7');
 
  
 button.addEventListener('click', () => {
     output1.textContent = input1.value;
-    output3.textContent = input1.value;
     output2.textContent = input2.value;
+    output3.textContent = input3.value;
+    output4.textContent = input4.value;
+    output5.textContent = input5.value;
+    output6.textContent = input6.value;
+    output7.textContent = input7.value;
     const div = document.getElementById('story');
     div.classList.remove('hidden');
 
